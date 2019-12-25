80f249745fb00c7b1725e6ef2fc2742b4cb3668b
Annie Huang <annie.hn@gmail.com>
2019-12-25 20:58:21 +1100

    <mat-autocomplete> component under ReactiveForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'material-demo';
     notifications = 0;
     showSpinner = false;
@@ -38,5 +41,17 @@ export class AppComponent {
     ];
     displayFn(subject) {
         return subject ? subject.name : undefined;
+    }
+    myControl = new FormControl();
+    filterOptions: Observable<string[]>;
+    ngOnInit(): void {
+        this.filterOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value))
+        );
+    }
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+        return this.options.filter(option => option.toLowerCase().includes(filterValue));
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from './material/material.module';
 
 @NgModule({
@@ -16,6 +16,7 @@ import {MaterialModule} from './material/material.module';
         AppRoutingModule,
         BrowserAnimationsModule,
         FormsModule,
+        ReactiveFormsModule,
         MaterialModule
     ],
     providers: [],
